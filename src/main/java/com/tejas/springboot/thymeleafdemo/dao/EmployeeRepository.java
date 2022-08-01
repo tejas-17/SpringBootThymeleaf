462bc95e7031da1cf36d303c7b2a0dcae70f5654
tejas nimbre <tejasnimbre99@gmail.com>
2022-08-02 04:04:05 +0530

    ThymeleafTejas pushed to github

---
+++ src/main/java/com/tejas/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.tejas.springboot.thymeleafdemo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tejas.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+	
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
+}
